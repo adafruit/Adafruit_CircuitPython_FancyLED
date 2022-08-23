22ffffcc47d691a6b90b7691efb69b5e631f2361
Alec Delaney <tekktik@gmail.com>
2022-08-23 17:26:22 -0400

    Use year duration range for copyright attribution

--- docs/conf.py
+++ docs/conf.py
@@ -37,8 +37,14 @@ master_doc = "index"
 
 # General information about the project.
 project = "Adafruit fancyled Library"
+creation_year = "2017"
 current_year = str(datetime.datetime.now().year)
-copyright = current_year + " PaintYourDragon"
+year_duration = (
+    current_year
+    if current_year == creation_year
+    else creation_year + " - " + current_year
+)
+copyright = year_duration + " PaintYourDragon"
 author = "PaintYourDragon"
 
 # The version info for the project you're documenting, acts as replacement for
